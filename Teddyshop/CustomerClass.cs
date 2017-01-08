d17ea17e97f527bf261d3478bb0330df889b1a39
guraa <gustav_egner@hotmail.com>
2017-01-08 13:24:50 +0100

    Initial commit

---
+++ Teddyshop/CustomerClass.cs
@@ -0,0 +1,28 @@
+﻿namespace Teddyshop
+{
+   class CustomerClass
+    {
+        public string CustomerNo { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int SSN { get; set; }
+        public string Gender { get; set; }
+        public string HomeAddress { get; set; }
+        public int HomeZipCode { get; set; }
+
+
+
+        public void customerClass(string customerNo)
+        {
+            this.CustomerNo = CustomerNo;
+            this.FirstName = FirstName;
+            this.LastName = LastName;
+            this.HomeAddress = HomeAddress;
+            this.HomeZipCode = HomeZipCode;
+            this.SSN = SSN;
+
+        }
+
+
+    }
+}
