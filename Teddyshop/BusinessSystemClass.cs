16aa2b200ef1c6bed479f535a69a3b1c8b89ed63
guraa <gustav_egner@hotmail.com>
2017-01-11 18:20:03 +0100

    Seperated BusinessSystem

---
+++ Teddyshop/BusinessSystemClass.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using Newtonsoft.Json.Linq;
+
+namespace Teddyshop
+{
+    class BusinessSystemClass<T> where T : CustomerClass
+    {
+        List<CustomerClass> listCustomers = new List<CustomerClass>();
+
+
+        public void InitiateCustomerBase() //Initiating 10 random customers to the business system using slumpa.net API
+        {
+
+            int i = 0;
+
+            while (i < 10)
+            {
+
+                string httpClient = new HttpClient().GetStringAsync("http://www.slumpa.net/api/").Result;
+                JObject token = JObject.Parse(httpClient);
+                CustomerClass NewPerson = new CustomerClass();
+                NewPerson.FirstName = (string)token["name"];
+                NewPerson.LastName = (string)token["lastname"];
+                NewPerson.CustomerNo = (string)token["ssn"];
+
+                listCustomers.Add(NewPerson);
+                i++;
+
+            }
+            customersInSystem();
+        }
+
+
+        public void customersInSystem()
+        {
+
+            List<CustomerClass>.Enumerator e = listCustomers.GetEnumerator(); //Loop through and send all the vehicles in the garage
+            Write(e);
+
+        }
+        public void registerNewCustomer()
+        {
+            CustomerClass ManualCustomerRegistration = new CustomerClass();
+            string systemInput = "0";
+
+            while (systemInput != "9")
+            {
+                Console.WriteLine("Please folow instructions below");
+                Console.Write("Firstname: ");
+                ManualCustomerRegistration.FirstName = Console.ReadLine();
+                Console.Write("Lastname: ");
+                ManualCustomerRegistration.LastName = Console.ReadLine();
+                Console.Write("Address : ");
+                ManualCustomerRegistration.HomeAddress = Console.ReadLine();
+                Console.Write("ZipCode: ");
+                ManualCustomerRegistration.HomeZipCode = Convert.ToInt32(Console.ReadLine());
+                Console.Write("Social security number : ");
+                ManualCustomerRegistration.SSN = Convert.ToInt32(Console.ReadLine());
+                ManualCustomerRegistration.CustomerNo = Console.ReadLine();
+
+                Console.WriteLine("\n You have provided the following details, would you like to add the customer?\n");
+
+                Console.Write("Y/N");
+                string answer = Console.ReadLine();
+
+                if (answer.ToLower() == "y")
+                {
+                    listCustomers.Add(ManualCustomerRegistration);
+
+                    Console.WriteLine("Customer: {0} was added", ManualCustomerRegistration.FirstName);
+
+                }
+                else if (answer.ToLower() == "n")
+                {
+                    Console.WriteLine("Customer registration was dismissed");
+                    systemInput = "9";
+                }
+                else
+                {
+                    Console.WriteLine("Please select either Y or N");
+                }
+
+
+                //this.CustomerNo = CustomerNo;
+                //this.FirstName = FirstName;
+                //this.LastName = LastName;
+                //this.HomeAddress = HomeAddress;
+                //this.HomeZipCode = HomeZipCode;
+                //this.SSN = SSN;
+
+
+            }
+
+
+        }
+
+        static void Write(IEnumerator<CustomerClass> e)
+        {
+            while (e.MoveNext()) //initiating enumerator to print all the vehicles in garage. MoveNext is a built in function that iterates the results.
+            {
+                CustomerClass value = e.Current;
+                Console.WriteLine(value.FirstName);
+                Console.WriteLine(value.LastName);
+                Console.WriteLine(value.CustomerNo);
+                Console.WriteLine(value.HomeAddress);
+            }
+
+
+
+        }
+    }
+}
